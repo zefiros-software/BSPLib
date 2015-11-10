cbca6e32ba22c0da9aa9c8d92103d0158f1c30b1
mick.v.duijn <mick.v.duijn@hotmail.com>
2015-11-10 21:30:30 +0100

    Reintroduced ConVarBarrier

--- bsp/bsp.h
+++ bsp/bsp.h
@@ -444,6 +444,67 @@ private:
         std::string msg;
     };
 
+    class CondVarBarrier
+    {
+    public:
+
+        explicit CondVarBarrier( std::size_t count )
+            : mCurrentCon( &mConVar1 ),
+              mPreviousCon( &mConVar2 ),
+              mCount( count ),
+              mMax( count )
+        {
+        }
+
+        void SetSize( size_t count )
+        {
+            mCount = count;
+            mMax = count;
+        }
+
+        void Wait( std::atomic_bool &aborted )
+        {
+            std::unique_lock<std::mutex> lock( mMutex );
+
+            if ( aborted )
+            {
+                mCurrentCon->notify_all();
+                throw BSPAbort( "Thread Exited" );
+            }
+
+            if ( --mCount == 0 )
+            {
+                Reset();
+
+            }
+            else
+            {
+                mCurrentCon->wait( lock );
+            }
+        }
+
+    private:
+        std::mutex mMutex;
+        std::condition_variable mConVar1;
+        std::condition_variable mConVar2;
+
+        std::condition_variable *mCurrentCon;
+        std::condition_variable *mPreviousCon;
+
+        size_t mCount;
+        size_t mMax;
+
+        void Reset()
+        {
+            mCount = mMax;
+            std::condition_variable *tmpCon = mCurrentCon;
+            mCurrentCon = mPreviousCon;
+            mPreviousCon = tmpCon;
+
+            tmpCon->notify_all();
+        }
+    };
+
     class Barrier
     {
     public:
