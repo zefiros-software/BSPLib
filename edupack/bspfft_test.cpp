5ea0d2062d0e7933c5b84a3df81f05fa3c8697e5
Paul Visscher <paul.pev@hotmail.com>
2015-11-28 16:59:44 +0100

    Fixed compiling

--- edupack/bspfft_test.cpp
+++ edupack/bspfft_test.cpp
@@ -45,7 +45,12 @@ void bspfft_test()
     if ( s == 0 )
     {
         printf( "Please enter length n: \n" );
+
+#ifdef _WIN32
         scanf_s( "%d", &n );
+#else
+        scanf( "%d", &n );
+#endif
 
         if ( n < 2 * p )
         {
@@ -180,12 +185,16 @@ void bspfft_test()
 
 int main( int argc, char **argv )
 {
-
     bsp_init( bspfft_test, argc, argv );
 
     printf( "How many processors do you want to use?\n" );
     fflush( stdout );
+
+#ifdef _WIN32
     scanf_s( "%d", &P );
+#else
+    scanf( "%d", &P );
+#endif
 
     if ( P > bsp_nprocs() )
     {
