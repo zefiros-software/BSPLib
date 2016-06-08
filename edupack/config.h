2f6fc4e85e1d3d683eeee32545bb417724da9033
mick.v.duijn <mick.v.duijn@hotmail.com>
2016-06-08 13:30:47 +0200

    Temporary armadillo config fix

--- edupack/config.h
+++ edupack/config.h
@@ -2,11 +2,9 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
-//#define ARMA_USE_MKL_ALLOC
-#define ARMA_USE_CXX11
-
 #pragma warning(push)
 #pragma warning(disable : 4702)
+#define ARMA_DONT_USE_BLAS
 #include <armadillo>
 #pragma warning(pop)
 
