c798c4277d5cb72193b6c36e83e4aa677a0703df
PaulVisscher <paul.pev@hotmail.com>
2016-06-11 10:09:04 +0200

    Coverage generation

--- generateCoverage.sh
+++ generateCoverage.sh
@@ -2,7 +2,7 @@ rm -rf gcov
 rm -rf target/site/clover
 mkdir gcov
 cd gcov/
-gcov --object-directory ../bin/obj/x32/Coverage/bsp-test/* ../test
+gcov --object-directory ../bin/obj/x86/Coverage/bsp-test/* ../test
 lcov --capture --base-directory ../test --directory .. --directory ../test/ --directory ../bsp/include/ --output-file coverage.info --no-external
 genhtml coverage.info --output-directory ../target/site/clover
-python  ../extern/gcov/gcov_to_clover.py *.gcov
+python  ../assets/Zefiros-Software/BSPLib/Zefiros-Software/gcov2clover/gcov2clover.py *.gcov
