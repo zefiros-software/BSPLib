90296545936b3bc428d4243a474ab6b0c1bf73a9
PaulVisscher <paul.pev@hotmail.com>
2016-08-07 13:17:41 +0200

    Appveyor support

---
+++ script/appveyor-build.bat
@@ -0,0 +1,19 @@
+premake5 install-package --allow-install --allow-module  || exit /b
+premake5 vs2015 || exit /b
+msbuild bsp/BSPLib.sln /property:Configuration=Release /property:Platform=Win32 || exit /b
+msbuild bsp/BSPLib.sln /property:Configuration=Debug /property:Platform=Win32 || exit /b
+msbuild bsp/BSPLib.sln /property:Configuration=Release /property:Platform=x64 || exit /b
+msbuild bsp/BSPLib.sln /property:Configuration=Debug /property:Platform=x64 || exit /b
+
+cd test/
+premake5 vs2015 || exit /b
+msbuild zpm/BSPLib-ZPM.sln || exit /b
+
+cd ../docs/images/ || exit /b
+..\..\bin\x86\bsp-test.exe || exit /b
+..\..\bin\x86\bsp-testd.exe || exit /b
+
+..\..\bin\x86_64\bsp-test.exe || exit /b
+..\..\bin\x86_64\bsp-testd.exe || exit /b
+
+..\..\test\bin\x86\bsp-zpm-test.exe || exit /b
