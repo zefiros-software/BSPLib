00f1200f48a5ca239d9513efcbf40df98205c51a
Paul Visscher <paul.pev@hotmail.com>
2016-08-09 10:56:26 +0200

    Disable debug builds

--- script/travis-build.sh
+++ script/travis-build.sh
@@ -3,8 +3,8 @@ set -e
 premake5 install-package --allow-install --allow-module
 premake5 gmake
 cd bsp
-make config=debug_x86
-make config=debug_x86_64
+#make config=debug_x86
+#make config=debug_x86_64
 make config=release_x86
 make config=release_x86_64
 #make config=coverage_x86
@@ -19,11 +19,11 @@ make
 cd ../../
 
 bin/x86/bsp-test
-bin/x86/bsp-testd
+#bin/x86/bsp-testd
 #bin/x86/bsp-testcd
 
 bin/x86_64/bsp-test
-bin/x86_64/bsp-testd
+#bin/x86_64/bsp-testd
 #bin/x86_64/bsp-testcd
 
 test/bin/x86/bsp-zpm-test
