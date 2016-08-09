ff056aa4afd638ee5f7812e306c8440979a6363c
Paul Visscher <paul.pev@hotmail.com>
2016-08-09 11:00:45 +0200

    Disabled wrong debug biuld

--- script/appveyor-build.bat
+++ script/appveyor-build.bat
@@ -1,9 +1,9 @@
 premake5 install-package --allow-install --allow-module  || exit /b
 premake5 vs2015 || exit /b
 msbuild bsp/BSPLib.sln /property:Configuration=Release /property:Platform=Win32 || exit /b
-msbuild bsp/BSPLib.sln /property:Configuration=Debug /property:Platform=Win32 || exit /b
+::msbuild bsp/BSPLib.sln /property:Configuration=Debug /property:Platform=Win32 || exit /b
 msbuild bsp/BSPLib.sln /property:Configuration=Release /property:Platform=x64 || exit /b
-msbuild bsp/BSPLib.sln /property:Configuration=Debug /property:Platform=x64 || exit /b
+::msbuild bsp/BSPLib.sln /property:Configuration=Debug /property:Platform=x64 || exit /b
 
 cd test/
 premake5 vs2015 || exit /b
@@ -12,9 +12,9 @@ msbuild zpm/BSPLib-ZPM.sln || exit /b
 cd ../
 
 bin\x86\bsp-test.exe || exit /b
-bin\x86\bsp-testd.exe || exit /b
+::bin\x86\bsp-testd.exe || exit /b
 
 bin\x86_64\bsp-test.exe || exit /b
-bin\x86_64\bsp-testd.exe || exit /b
+::bin\x86_64\bsp-testd.exe || exit /b
 
 test\bin\x86\bsp-zpm-test.exe || exit /b
