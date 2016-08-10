62d9ba8f3213c4cdd51875c90fa3f4492ebf377b
Paul Visscher <paul.pev@hotmail.com>
2016-08-10 15:03:13 +0200

    Fix mkdocs path

--- script/deploy-docs.sh
+++ script/deploy-docs.sh
@@ -3,4 +3,4 @@ git config user.name "Paul Visscher"
 git config user.email "p.e.visscher@zefiros.eu"
 git remote add gh-token "https://${GH_TOKEN}@github.com/Zefiros-Software/BSPLib.git";
 git fetch gh-token && git fetch gh-token gh-pages:gh-pages;
-mkdocs gh-deploy -v --clean --remote-name gh-token;
+~/zpm-anaconda/bin/mkdocs gh-deploy -v --clean --remote-name gh-token;
