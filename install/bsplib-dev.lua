309942e50368d15ebfda2eff81471f04415f4aea
PaulVisscher <paul.pev@hotmail.com>
2016-08-13 11:18:48 +0200

    Anaconda asset to modules migration

--- install/bsplib-dev.lua
+++ install/bsplib-dev.lua
@@ -22,18 +22,10 @@
 -- @endcond
 --]]
 
-dofile( "assets/Zefiros-Software/BSPLib/Zefiros-Software/Anaconda/anaconda.lua" )
+local ana = require( "Zefiros-Software/Anaconda", "@head" )
 
-local anaBin = os.get() == "windows" and os.getenv("UserProfile") .. "/zpm-anaconda/Scripts/" or "~/zpm-anaconda/bin/"
-
-if os.get() == "windows" then
-    os.executef( "%spip install mkdocs -U", anaBin )
-    os.executef( "%spip install mkdocs-bootswatch -U", anaBin )
-    os.executef( "%spip install pymdown-extensions -U", anaBin )
-    os.executef( "%spip install markdown-checklist -U", anaBin )
-else
-    os.executef( "%s/python3 %spip install mkdocs -U", anaBin, anaBin )
-    os.executef( "%s/python3 %spip install mkdocs-bootswatch -U", anaBin, anaBin )
-    os.executef( "%s/python3 %spip install pymdown-extensions -U", anaBin, anaBin )
-    os.executef( "%s/python3 %spip install markdown-checklist -U", anaBin, anaBin )
-end
+ana.install()
+ana.pip( "install mkdocs -U" )
+ana.pip( "install mkdocs-bootswatch -U" )
+ana.pip( "install pymdown-extensions -U" )
+ana.pip( "install markdown-checklist -U" )
