badd68f642a5c9d505748e421827c3c622f7039a
PaulVisscher <paul.pev@hotmail.com>
2016-08-15 01:31:54 +0200

    Error code

--- script/appveyor-build.bat
+++ script/appveyor-build.bat
@@ -1,20 +1,20 @@
-premake5 install-package --allow-install --allow-module  || exit /b
-premake5 vs2015 || exit /b
-msbuild bsp/BSPLib.sln /property:Configuration=Release /property:Platform=Win32 || exit /b
-::msbuild bsp/BSPLib.sln /property:Configuration=Debug /property:Platform=Win32 || exit /b
-msbuild bsp/BSPLib.sln /property:Configuration=Release /property:Platform=x64 || exit /b
-::msbuild bsp/BSPLib.sln /property:Configuration=Debug /property:Platform=x64 || exit /b
+premake5 install-package --allow-install --allow-module  || exit /b 1
+premake5 vs2015 || exit /b 1
+msbuild bsp/BSPLib.sln /property:Configuration=Release /property:Platform=Win32 || exit /b 1
+::msbuild bsp/BSPLib.sln /property:Configuration=Debug /property:Platform=Win32 || exit /b 1
+msbuild bsp/BSPLib.sln /property:Configuration=Release /property:Platform=x64 || exit /b 1
+::msbuild bsp/BSPLib.sln /property:Configuration=Debug /property:Platform=x64 || exit /b 1
 
 cd test/
-premake5 vs2015 || exit /b
-msbuild zpm/BSPLib-ZPM.sln || exit /b
+premake5 vs2015 || exit /b 1
+msbuild zpm/BSPLib-ZPM.sln || exit /b 1
 
 cd ../
 
-bin\x86\bsp-test.exe || exit /b
-::bin\x86\bsp-testd.exe || exit /b
+bin\x86\bsp-test.exe || exit /b 1
+::bin\x86\bsp-testd.exe || exit /b 1
 
-bin\x86_64\bsp-test.exe || exit /b
-::bin\x86_64\bsp-testd.exe || exit /b
+bin\x86_64\bsp-test.exe || exit /b 1
+::bin\x86_64\bsp-testd.exe || exit /b 1
 
-test\bin\x86\bsp-zpm-test.exe || exit /b
+test\bin\x86\bsp-zpm-test.exe || exit /b 1
