a6fbd9d575a3b19bc57b424cef029ed9b47401a7
PaulVisscher <paul.pev@hotmail.com>
2016-08-15 09:47:18 +0200

    License added

--- generateCoverage.sh
+++
@@ -1,8 +0,0 @@
-rm -rf gcov
-rm -rf target/site/clover
-mkdir gcov
-cd gcov/
-gcov --object-directory ../bin/obj/x86/Coverage/bsp-test/* ../test
-lcov --capture --base-directory ../test --directory .. --directory ../test/ --directory ../bsp/include/ --output-file coverage.info --no-external
-genhtml coverage.info --output-directory ../target/site/clover
-python  ../assets/Zefiros-Software/BSPLib/Zefiros-Software/gcov2clover/gcov2clover.py *.gcov
